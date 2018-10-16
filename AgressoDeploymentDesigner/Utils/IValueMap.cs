1a41c1ad9d76ac39e9c61d8fb988651a58aa31f6
Jan Klincak <jan.klincak@nxtsoft.sk>
2018-10-16 21:52:57 +0200

    Initial project commit.

---
+++ AgressoDeploymentDesigner/Utils/IValueMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgressoDeploymentDesigner.Utils
+{
+	/// <summary>
+	/// Provides mapping of Agresso defined values to a human readable ones.
+	/// </summary>
+	/// <typeparam name="T">Type of mapped value.</typeparam>
+	public interface IValueMap<T>
+	{
+
+	}
+}
